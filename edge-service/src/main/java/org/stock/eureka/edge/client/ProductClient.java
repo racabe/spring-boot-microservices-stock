cbc267dcaf2ba92dfb01c4db682398b1d53034f8
racabe <racabe@users.noreply.github.com>
2018-05-23 03:21:05 +0200

    Add the methods findById and updateProduct

--- edge-service/src/main/java/org/stock/eureka/edge/client/ProductClient.java
+++ edge-service/src/main/java/org/stock/eureka/edge/client/ProductClient.java
@@ -2,7 +2,11 @@ package org.stock.eureka.edge.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.stock.eureka.edge.model.Product;
 
 /**
@@ -12,10 +16,30 @@ import org.stock.eureka.edge.model.Product;
 public interface ProductClient {
 
   /**
+   * Find by id.
+   *
+   * @param idProduct the id product
+   * @return the product
+   */
+  @GetMapping("/products/{id}")
+  Product findById(@PathVariable("id") Long idProduct);
+
+  /**
    * Read products.
    *
    * @return the resources
    */
   @GetMapping("/products")
   Resources<Product> readProducts();
+
+  /**
+   * Update product.
+   *
+   * @param idProduct the id product
+   * @param currentProduct the current product
+   * @return the response entity
+   */
+  @PutMapping("/products/{id}")
+  ResponseEntity<Product> updateProduct(@PathVariable("id") Long idProduct,
+      @RequestBody Product currentProduct);
 }
